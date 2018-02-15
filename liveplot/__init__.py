aedc47a2f270d8c0db62a51cc46c84341588ec9c
rsoklaski <ryan.soklaski@ll.mit.edu>
2018-02-15 10:27:19 -0500

    add save/load options

--- liveplot/__init__.py
+++ liveplot/__init__.py
@@ -1,5 +1,3 @@
-from liveplot.plotter import LivePlot
-
 __all__ = ["create_plot", "recreate_plot"]
 
 def create_plot(metrics, refresh=0., plot_title=None, figsize=None, track_time=True):
@@ -35,9 +33,10 @@ def create_plot(metrics, refresh=0., plot_title=None, figsize=None, track_time=T
 
         Examples
         --------
+        Creating a live plot in a Jupyter notebook
         >>> %matplotlib notebook
         >>> import numpy as np
-        >>> from liveplot import create_plot
+        >>> from liveplot import create_plot, save_metrics
         >>> metrics = ["accuracy", "loss"]
         >>> plotter, fig, ax = create_plot(metrics, refresh=0)
         >>> for i, x in enumerate(np.linspace(0, 10, 100)):
@@ -58,21 +57,26 @@ def create_plot(metrics, refresh=0., plot_title=None, figsize=None, track_time=T
         ...         plotter.plot_test_epoch()
         ...
         ... plotter.plot()  # ensures final data gets plotted
+        >>> save_metrics("./metrics.npz", plotter) # save metrics to numpy-archive
 
-        """
+        Loading and recreating plot
+        >>> from liveplot import load_metrics, recreate_plot
+        >>> train, test = load_metrics("./metrics.npz")
+        >>> recreate_plot(train_metrics=train, test_metrics=test)"""
 
+    from liveplot.plotter import LivePlot
     live_plotter = LivePlot(metrics, refresh, plot_title, figsize, track_time)
     live_plotter._init_plot_window()
     fig, ax = live_plotter.plot_objects()
     return live_plotter, fig, ax
 
 
-def recreate_plot(old_plot=None, train_metrics=None, test_metrics=None):
+def recreate_plot(liveplot=None, *, train_metrics=None, test_metrics=None):
     """ Recreate a plot from a LivePlot instance or from train/test metric dictionaries.
 
         Parameters
         ----------
-        old_plot : Optional[liveplot.LivePlot]
+        liveplot : Optional[liveplot.LivePlot]
             An existing liveplot object.
 
         train_metrics : Optional[OrderedDict[str, Dict[str, numpy.ndarray]]]
@@ -90,8 +94,8 @@ def recreate_plot(old_plot=None, train_metrics=None, test_metrics=None):
         Tuple[liveplot.LivePlot, matplotlib.figure.Figure, numpy.ndarray(matplotlib.axes.Axes)]
             (LivePlot-instance, figure, array-of-axes)"""
     from liveplot.plotter import LiveMetric
-    assert any(i is not None for i in [old_plot, train_metrics, test_metrics])
-    if old_plot is not None:
+    assert any(i is not None for i in [liveplot, train_metrics, test_metrics])
+    if liveplot is not None:
         assert train_metrics is None and test_metrics is None
     else:
         if train_metrics is None:
@@ -99,27 +103,27 @@ def recreate_plot(old_plot=None, train_metrics=None, test_metrics=None):
         if test_metrics is None:
             test_metrics = {}
 
-    if old_plot:
-        metrics = old_plot._metrics
+    if liveplot:
+        metrics = liveplot._metrics
     else:
         metrics = list(train_metrics)
         metrics.extend(k for k in test_metrics if k not in metrics)
 
     new, fig, ax = create_plot(metrics, refresh=-1)
 
-    if old_plot:
-        new._metric_colors = old_plot._metric_colors
+    if liveplot:
+        new._metric_colors = liveplot._metric_colors
 
     new._init_plot_window()
     fig, ax = new.plot_objects()
 
-    if old_plot:
-        new._train_metrics = old_plot._train_metrics
-        new._test_metrics = old_plot._test_metrics
+    if liveplot:
+        new._train_metrics = liveplot._train_metrics
+        new._test_metrics = liveplot._test_metrics
 
         for attr in ["_train_epoch_num", "_train_batch_num",
                      "_test_epoch_num", "_test_batch_num"]:
-            setattr(new, attr, getattr(old_plot, attr))
+            setattr(new, attr, getattr(liveplot, attr))
     else:
         new._train_metrics.update((key, LiveMetric(key)) for key in train_metrics if key in new._metrics)
         new._test_metrics.update((key, LiveMetric(key)) for key in test_metrics if key in new._metrics)
@@ -171,3 +175,82 @@ def recreate_plot(old_plot=None, train_metrics=None, test_metrics=None):
 
     new.plot()
     return new, fig, ax
+
+def save_metrics(path, liveplot=None, *, train_metrics=None, test_metrics=None):
+    """ Save live-plot metrics to a numpy zipped-archive (.npz). A LivePlot-instance
+        can be supplied, or train/test metrics can be passed explicitly to the function.
+
+        Parameters
+        ----------
+        path: PathLike
+           The file-path used to save the archive. E.g. 'path/to/saved_metrics.npz'
+
+        liveplot : Optional[liveplot.LivePlot]
+           The LivePlot instance whose metrics will be saves.
+
+        train_metrics : Optional[OrderedDict[str, Dict[str, numpy.ndarray]]]]
+
+            metric-name -> {batch_data -> array, epoch_domain -> array, epoch_data -> array}
+
+        test_metrics : Optional[OrderedDict[str, Dict[str, numpy.ndarray]]]]
+            metric-name -> {batch_data -> array, epoch_domain -> array, epoch_data -> array}"""
+    import numpy as np
+
+    if liveplot is not None:
+        train_metrics = liveplot.train_metrics
+        test_metrics = liveplot.test_metrics
+    else:
+        if train_metrics is None:
+            train_metrics = {}
+
+        if test_metrics is None:
+            test_metrics = {}
+
+    # use unique separator
+    sep = ";"
+    names = "".join(tuple(train_metrics) + tuple(test_metrics))
+    while sep in names:
+        sep += ";"
+
+    # flatten metrics to single mapping
+    save_dict = {}  # train/test;metric_name;metric_data -> array
+    for type_, metrics in zip(["train", "test"], [train_metrics, test_metrics]):
+        for name, metric in metrics.items():
+            save_dict.update({sep.join((type_, name, k)): v for k, v in metric.items()})
+
+    with open(path, "wb") as f:
+        np.savez(f, train_order=list(train_metrics), test_order=list(test_metrics),
+                 sep=sep, **save_dict)
+
+
+def load_metrics(path):
+    """ Load liveplot metrics from a numpy archive.
+
+        Parameters
+        ----------
+        path : PathLike
+            Path to numpy archive.
+
+        Returns
+        -------
+        Tuple[OrderedDict[str, Dict[str, numpy.ndarray]], OrderedDict[str, Dict[str, numpy.ndarray]]]]
+           (train-metrics, test-metrics)"""
+    from collections import OrderedDict, defaultdict
+    import numpy as np
+
+    recursive_default_dict = lambda: defaultdict(recursive_default_dict)
+    out = recursive_default_dict()
+
+    with np.load(path) as f:
+        data_dict = dict(f)
+
+    train_order = list(data_dict.pop("train_order"))
+    test_order = list(data_dict.pop("test_order"))
+    sep = data_dict.pop("sep").item()
+    for k, v in data_dict.items():
+        type_, metric_name, data_type = k.split(sep)
+        out[type_][metric_name][data_type] = v
+
+    train_metrics = OrderedDict(((k, dict(out["train"][k])) for k in train_order))
+    test_metrics = OrderedDict(((k, dict(out["test"][k])) for k in test_order))
+    return train_metrics, test_metrics
