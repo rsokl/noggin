a1e0d93eef281f5e1b6940c3dc35c4a40031d861
Ryan Soklaski <ry26099@mit.edu>
2019-03-16 12:20:06 -0400

    add get_xarray test

--- tests/custom_strategies.py
+++ tests/custom_strategies.py
@@ -26,7 +26,8 @@ def choices(seq: Sequence, size: int) -> st.SearchStrategy[Tuple]:
 
 
 @st.composite
-def metric_dict(draw, num_batch_data=None,
+def metric_dict(draw, name,
+                num_batch_data=None,
                 num_epoch_data=None,
                 epoch_domain=None) -> st.SearchStrategy[Dict[str, np.ndarray]]:
     if all(x is not None for x in (num_batch_data, num_epoch_data)):
@@ -41,7 +42,7 @@ def metric_dict(draw, num_batch_data=None,
     if epoch_domain is None:
         epoch_domain = draw(choices(np.arange(1, num_batch_data + 1), size=num_epoch_data))
 
-    out = {}  # type: Dict[str, np.ndarray]
+    out = dict(name=name)  # type: Dict[str, np.ndarray]
     out["batch_data"] = draw(finite_array(num_batch_data))  # type: np.ndarray
     out["epoch_data"] = draw(finite_array(num_epoch_data))  # type: np.ndarray
     out["epoch_domain"] = np.asarray(sorted(epoch_domain))
@@ -59,15 +60,23 @@ def live_metrics(draw) -> st.SearchStrategy[LiveMetrics]:
 
     out = defaultdict(dict)  # type: Dict[str, Dict[str, np.ndarray]]
     for name in ["metric_a", "metric_b", "metric_c"][:num_metrics]:
-        out[name] = draw(metric_dict(num_batch_data=num_batch_data,
+        out[name] = draw(metric_dict(name,
+                                     num_batch_data=num_batch_data,
                                      num_epoch_data=num_epoch_data,
                                      ))
     return dict(out.items())
+
 
 @st.composite
 def logger(draw) -> st.SearchStrategy[LiveLogger]:
     train_metrics = draw(live_metrics())
     test_metrics = draw(live_metrics())
-
-
-
+    return LiveLogger.from_dict(
+        dict(train_metrics=train_metrics,
+             test_metrics=test_metrics,
+             num_train_epoch=max((len(v["epoch_data"]) for v in train_metrics.values()), default=0),
+             num_train_batch=max((len(v["batch_data"]) for v in train_metrics.values()), default=0),
+             num_test_epoch=max((len(v["epoch_data"]) for v in test_metrics.values()), default=0),
+             num_test_batch=max((len(v["batch_data"]) for v in test_metrics.values()), default=0),
+             )
+    )
--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -1,5 +1,6 @@
 from liveplot.xarray import metrics_to_xarrays, get_xarrays
 from liveplot.typing import LiveMetrics
+from liveplot.logger import LiveLogger
 
 import tests.custom_strategies as cst
 
@@ -9,6 +10,10 @@ from hypothesis import given
 
 
 def check_batch_xarray(metrics_dict, metrics_xarray):
+    if not metrics_dict:
+        assert not metrics_xarray
+        return
+
     # tests for batch-level data
     assert list(metrics_xarray.data_vars) == list(metrics_dict)
     num_iterations = max(len(v["batch_data"]) for v in metrics_dict.values())
@@ -23,6 +28,10 @@ def check_batch_xarray(metrics_dict, metrics_xarray):
 
 
 def check_epoch_xarray(metrics_dict, metrics_xarray):
+    if not metrics_dict:
+        assert not metrics_xarray
+        return
+
     # tests for epoch-level data
     assert list(metrics_xarray.data_vars) == list(metrics_dict)
 
@@ -42,13 +51,17 @@ def check_epoch_xarray(metrics_dict, metrics_xarray):
 @given(metrics=cst.live_metrics())
 def test_metrics_to_xarrays(metrics: LiveMetrics):
     batch_xr, epoch_xr = metrics_to_xarrays(metrics)
-    if not metrics:
-        assert not batch_xr
-        assert not epoch_xr
-        return
-
     check_batch_xarray(metrics_dict=metrics, metrics_xarray=batch_xr)
     check_epoch_xarray(metrics_dict=metrics, metrics_xarray=epoch_xr)
 
 
+@given(logger=cst.logger())
+def test_get_xarray(logger: LiveLogger):
+    out = get_xarrays(logger)
+    tr_xr = out['train']
+    te_xr = out['test']
 
+    check_batch_xarray(logger.train_metrics, tr_xr["batch"])
+    check_epoch_xarray(logger.train_metrics, tr_xr["epoch"])
+    check_batch_xarray(logger.test_metrics, te_xr["batch"])
+    check_epoch_xarray(logger.test_metrics, te_xr["epoch"])
