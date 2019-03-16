847321102d5a59ed37681da3a955b4b9fe901c38
Ryan Soklaski <ry26099@mit.edu>
2019-03-16 11:47:03 -0400

    improve search strats and reorg xarray test

--- tests/test_xarray.py
+++ tests/test_xarray.py
@@ -8,6 +8,37 @@ from numpy.testing import assert_array_equal
 from hypothesis import given
 
 
+def check_batch_xarray(metrics_dict, metrics_xarray):
+    # tests for batch-level data
+    assert list(metrics_xarray.data_vars) == list(metrics_dict)
+    num_iterations = max(len(v["batch_data"]) for v in metrics_dict.values())
+    assert_array_equal(metrics_xarray.coords["iterations"], np.arange(1, num_iterations + 1))
+
+    for name, data in metrics_dict.items():
+        assert_array_equal(
+            getattr(metrics_xarray, name),
+            data["batch_data"],
+            err_msg="(batch) {name} data does not match between the "
+                    "xarray and the original metric".format(name=name))
+
+
+def check_epoch_xarray(metrics_dict, metrics_xarray):
+    # tests for epoch-level data
+    assert list(metrics_xarray.data_vars) == list(metrics_dict)
+
+    for name, data in metrics_dict.items():
+        xray = getattr(metrics_xarray, name)
+        nan_mask = np.logical_not(np.isnan(xray))
+        xray_epoch_domain = metrics_xarray.coords["iterations"][nan_mask]
+        assert_array_equal(xray_epoch_domain, metrics_dict[name]["epoch_domain"])
+
+        assert_array_equal(
+            xray[nan_mask],
+            data["epoch_data"],
+            err_msg="(epoch) {name} data does not match between the "
+                    "xarray and the original metric".format(name=name))
+
+
 @given(metrics=cst.live_metrics())
 def test_metrics_to_xarrays(metrics: LiveMetrics):
     batch_xr, epoch_xr = metrics_to_xarrays(metrics)
@@ -16,27 +47,8 @@ def test_metrics_to_xarrays(metrics: LiveMetrics):
         assert not epoch_xr
         return
 
-    # tests for batch-level data
-    assert list(batch_xr.data_vars) == list(metrics)
-    num_iterations = max(len(v["batch_data"]) for v in metrics.values())
-    assert_array_equal(batch_xr.coords["iterations"], np.arange(1, num_iterations + 1))
+    check_batch_xarray(metrics_dict=metrics, metrics_xarray=batch_xr)
+    check_epoch_xarray(metrics_dict=metrics, metrics_xarray=epoch_xr)
 
-    for name, data in metrics.items():
-        assert_array_equal(
-            getattr(batch_xr, name),
-            data["batch_data"],
-            err_msg="(batch) {name} data does not match between the "
-                    "xarray and the original metric".format(name=name))
 
-    # tests for batch-level data
-    assert list(epoch_xr.data_vars) == list(metrics)
-    key = list(metrics.keys())[0]
-    epoch_iterations = metrics[key]["epoch_domain"]
-    assert_array_equal(epoch_xr.coords["iterations"], epoch_iterations)
 
-    for name, data in metrics.items():
-        assert_array_equal(
-            getattr(epoch_xr, name),
-            data["epoch_data"],
-            err_msg="(epoch) {name} data does not match between the "
-                    "xarray and the original metric".format(name=name))
