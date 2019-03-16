6c7f8dd094a35356facba4872335ca870028b5b3
Ryan Soklaski <ry26099@mit.edu>
2019-03-16 12:46:48 -0400

    add tests for custom strategies

---
+++ tests/custom_strategies/test_custom_strategies.py
@@ -0,0 +1,29 @@
+import tests.custom_strategies as cst
+from hypothesis import given
+
+from liveplot.logger import LiveLogger, LiveMetric
+from itertools import combinations
+
+
+@given(cst.choices("abcdefg", 3))
+def test_choices(choice):
+    assert choice in set(combinations("abcdefg", 3))
+
+
+@given(logger=cst.loggers())
+def test_loggers(logger: LiveLogger):
+    """Ensure that loggers() can produce a Logger that can round-trip"""
+    LiveLogger.from_dict(logger.to_dict())
+
+
+@given(metrics_dict=cst.metric_dict("metric-a"))
+def test_metrics_dict(metrics_dict: dict):
+    """Ensure that metrics_dict() can round-trip via LiveMetric"""
+    LiveMetric.from_dict(metrics_dict).to_dict()
+
+
+@given(live_metrics=cst.live_metrics())
+def test_livemetrics(live_metrics: dict):
+    """Ensure that each entry in live_metrics() can round-trip via LiveMetric"""
+    for metric_name, metrics_dict in live_metrics.items():
+        LiveMetric.from_dict(metrics_dict).to_dict()
