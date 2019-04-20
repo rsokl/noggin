3f7e54d17fed6431eb228761fae37839fd903246
Ryan Soklaski <ry26099@mit.edu>
2019-04-20 12:02:37 -0400

    fix valid color check; add color tests

--- liveplot/utils.py
+++ liveplot/utils.py
@@ -6,8 +6,8 @@ __all__ = ["check_valid_color"]
 
 def check_valid_color(c: ValidColor) -> bool:
     """
-    Checks if `c` is a valid color argument for matplotlib. Raises
-    `ValueError` if `c` is not a valid color.
+    Checks if `c` is a valid color argument for matplotlib or `None`.
+    Raises `ValueError` if `c` is not a valid color.
 
     Parameters
     ----------
@@ -22,7 +22,7 @@ def check_valid_color(c: ValidColor) -> bool:
     ValueError"""
     from matplotlib.colors import is_color_like
 
-    if not is_color_like(c):
+    if c is not None and not is_color_like(c):
         raise ValueError("{} is not a valid matplotlib color".format(repr(c)))
     else:
         return True
