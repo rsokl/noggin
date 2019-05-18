0f5dd4f4a6fd146b15443af0566ca7c6d37e59d2
Ryan Soklaski <ry26099@mit.edu>
2019-05-18 14:49:08 -0400

    add input validation tests

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def do_setup():
     setup(
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
-        name="LivePlot",
+        name="liveplot",
         author="Ryan Soklaski",
         license="MIT",
         platforms=["Windows", "Linux", "Mac OS-X", "Unix"],
