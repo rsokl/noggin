e5bb5fa486ea8a9e64020587e4579bcc4a6223a7
Ryan Soklaski <ry26099@mit.edu>
2019-05-25 17:27:37 -0400

    improve adaptive plotting tests

--- tests/test_adaptive_plotting.py
+++ tests/test_adaptive_plotting.py
@@ -4,17 +4,28 @@ from liveplot import LivePlot
 from time import sleep, time
 
 
+class ControlledPlot(LivePlot):
+    """Mocks out plotting. Forces plotting to take a specified
+    time; no plots are created"""
+
+    def __init__(self, *args, plot_time, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.plot_time = plot_time
+        self._liveplot = True
+
+    def _init_plot_window(self):
+        return
+
+    def plot(self):
+        sleep(self.plot_time)
+
+
 @pytest.mark.parametrize("plot_time", [0.0001, 0.001, 0.01])
 @pytest.mark.parametrize("outer_time", [0.0001, 0.001])
 @pytest.mark.parametrize("max_fraction", [0.0, 0.01, 0.3, 0.5])
 def test_adaptive_plotting(plot_time, outer_time, max_fraction):
-    def mock_plot():
-        sleep(plot_time)
+    plotter = ControlledPlot(metrics="a", plot_time=plot_time)
 
-    plotter = LivePlot(metrics="a")
-    plotter.plot = mock_plot
-    plotter._init_plot_window = lambda: None
-    plotter._liveplot = True
     plotter._max_fraction_spent_plotting = max_fraction
     total_plot_time = 0.0
 
@@ -33,24 +44,18 @@ def test_adaptive_plotting(plot_time, outer_time, max_fraction):
 
 
 @pytest.mark.parametrize(
-    ("plot_time", "outer_time", "expected_fraction"),
-    [(0.1, 0.001, 1.0), (0.001, 0.001, 0.5)],
+    ("plot_time", "outer_time", "max_fraction", "expected_fraction"),
+    [(0.1, 0.001, 1.0, 1.0), (0.1, 0.001, 0.7, 0.7), (0.001, 0.001, 1.0, 0.5)],
 )
-def test_exhaustive_plotting(plot_time, outer_time, expected_fraction):
+def test_exhaustive_plotting(plot_time, outer_time, max_fraction, expected_fraction):
     """Ensure that plotting dominates runtime when `max_fraction_spent_plotting` is 1
     and plotting is dominant.
 
     If plotting and outer-loop time are comparable, the fraction of time spent
     plotting should be ~0.5"""
 
-    def mock_plot():
-        sleep(plot_time)
-
-    plotter = LivePlot(metrics="a")
-    plotter.plot = mock_plot
-    plotter._init_plot_window = lambda: None
-    plotter._liveplot = True
-    plotter._max_fraction_spent_plotting = 1.0
+    plotter = ControlledPlot(metrics="a", plot_time=plot_time)
+    plotter._max_fraction_spent_plotting = max_fraction
     total_plot_time = 0.0
 
     start_time = time()
