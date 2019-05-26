a6459ed6c7c65eb07546acce7f7a9f4f81c7fb24
Ryan Soklaski <ry26099@mit.edu>
2019-05-25 20:09:05 -0400

    improve adaptivity testing

--- tests/test_adaptive_plotting.py
+++ tests/test_adaptive_plotting.py
@@ -22,16 +22,20 @@ class ControlledPlot(LivePlot):
 
 @pytest.mark.parametrize("plot_time", [0.0001, 0.001, 0.01])
 @pytest.mark.parametrize("outer_time", [0.0001, 0.001])
-@pytest.mark.parametrize("max_fraction", [0.0, 0.01, 0.3, 0.5])
+@pytest.mark.parametrize("max_fraction", [0.0, 0.01, 0.2, 0.4])
 def test_adaptive_plotting(plot_time, outer_time, max_fraction):
     plotter = ControlledPlot(metrics="a", plot_time=plot_time)
 
     plotter._max_fraction_spent_plotting = max_fraction
+    plotter._queue_size = 1
     total_plot_time = 0.0
 
     start_time = time()
     for n in range(500):
-        # continue
+        if n == 250:
+            # bump up plot time to ensure that plot rate
+            # adapts appropriately
+            plotter.plot_time = 10 * plot_time
         start_plot = time()
         plotter.set_train_batch(dict(a=1), batch_size=1, plot=True)
         total_plot_time += time() - start_plot
@@ -40,12 +44,17 @@ def test_adaptive_plotting(plot_time, outer_time, max_fraction):
 
     actual_fraction = total_plot_time / total_time
 
-    assert isclose(actual_fraction, max_fraction, rel_tol=0.2, abs_tol=0.01)
+    assert isclose(actual_fraction, max_fraction, rel_tol=0.3, abs_tol=0.01)
 
 
 @pytest.mark.parametrize(
     ("plot_time", "outer_time", "max_fraction", "expected_fraction"),
-    [(0.1, 0.001, 1.0, 1.0), (0.1, 0.001, 0.7, 0.7), (0.001, 0.001, 1.0, 0.5)],
+    [
+        (0.1, 0.001, 1.0, 1.0),
+        (0.1, 0.001, 0.7, 0.7),
+        (0.1, 0.001, 0.5, 0.5),
+        (0.001, 0.001, 1.0, 0.5),
+    ],
 )
 def test_exhaustive_plotting(plot_time, outer_time, max_fraction, expected_fraction):
     """Ensure that plotting dominates runtime when `max_fraction_spent_plotting` is 1
